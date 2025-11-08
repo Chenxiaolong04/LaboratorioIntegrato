8832aa5f659237c7281a9c5ad848fb3b24b3b2e6
n3dydr4gos <dragos.nedelcu@edu-its.it>
2025-11-08 17:12:56 +0100

    fix: added only logo instead of navbar and adjusted the style a bit

--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,16 +1,13 @@
-import { useLocation } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
-  const location = useLocation();
-  const hideFooter = location.pathname === "/login"; // controlla la route
 
   return (
     <>
       <Navbar />
       {children}
-      {!hideFooter && <Footer />} {/* mostra solo se non è la login */}
+      <Footer />
     </>
   );
 }
--- src/pages/public/Login.tsx
+++ src/pages/public/Login.tsx
@@ -1,53 +1,51 @@
 import { useState } from "react";
-import PublicLayout from "../../layouts/PublicLayout";
-import "../../assets/styles/_login.scss";
+import PrivateLayout from "../../layouts/PrivateLayout";
+import TestImage from "../../assets/img/test-image.jpg";
 
 export default function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        console.log("Email:", email);
-        console.log("Password:", password);
-        // TODO: aggiungi qui logica di autenticazione o redirect
-    };
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Email:", email);
+    console.log("Password:", password);
+    // TODO: aggiungi qui logica di autenticazione o redirect
+  };
 
-    return (
-        <PublicLayout>
-            <section className="login-page">
-                <header>
-                    <h1>
-                        Bentornato! <br /> Felice di rivederti
-                    </h1>
-                </header>
-
-                <form className="login-form" onSubmit={handleSubmit}>
-                    <div className="input-button">
-                        <input
-                            type="email"
-                            placeholder="Inserisci la tua email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                        />
-                    </div>
-
-                    <div className="input-button">
-                        <input
-                            type="password"
-                            placeholder="Inserisci la tua password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                        />
-                    </div>
-
-                    <button type="submit" className="login-submit">
-                        Accedi
-                    </button>
-                </form>
-            </section>
-        </PublicLayout>
-    );
+  return (
+    <PrivateLayout>
+      <section className="login-page">
+        <img src={TestImage} alt="" />
+        <div className="login-container">
+            <h1>
+              Bentornato! <br /> Felice di rivederti
+            </h1>
+            <form className="login-form" onSubmit={handleSubmit}>
+              <div className="input-button">
+                <input
+                  type="email"
+                  placeholder="Inserisci la tua email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className="input-button">
+                <input
+                  type="password"
+                  placeholder="Inserisci la tua password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <button type="submit" className="login-submit">
+                Accedi
+              </button>
+            </form>
+        </div>
+      </section>
+    </PrivateLayout>
+  );
 }
