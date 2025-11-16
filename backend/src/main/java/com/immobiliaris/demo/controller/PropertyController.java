afdea4debf3966c870cb49213a0a54072ef7aa6d
AngJim <angelo.jimenez@edu-its.it>
2025-11-16 14:37:53 +0100

    Refactor: sostituito Immobile con Property e aggiunto PropertyController con DTO
    - Rimossi ImmobileController, ImmobileService, ImmobileRepository e model Immobile
    - Aggiunto PropertyController con endpoint POST per aggiungere proprietà
    - Implementato PropertyRequest DTO per la gestione dei dati in ingresso
    - Integrato PropertyService per la logica di business

---
+++ backend/src/main/java/com/immobiliaris/demo/controller/PropertyController.java
@@ -0,0 +1,31 @@
+package com.immobiliaris.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.immobiliaris.demo.dto.PropertyRequest;
+import com.immobiliaris.demo.model.Property;
+import com.immobiliaris.demo.service.PropertyService;
+
+@RestController
+@RequestMapping("/api/property")
+@CrossOrigin(origins = "http://localhost:3000")
+public class PropertyController {
+    
+    private final PropertyService service;
+
+    public PropertyController (PropertyService service){
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> addProperty (@RequestBody PropertyRequest request){
+        Property propertyCreating = service.createProperty(request);
+        return ResponseEntity.ok(propertyCreating);
+    }
+    
+}
